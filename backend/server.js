a25d83ac35053aa481f4f85786392868b0755568
AlfredhoPranoto <alfredhopranoto@gmail.com>
2025-03-24 15:55:47 +0700

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { connectDB } from "./config/db.js";
+import cors from "cors";
+import productRoutes from "./routes/product.route.js";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+const origins = process.env.CORS_ORIGINS
+  ? process.env.CORS_ORIGINS.split(",")
+  : ["https://mern-product-management.vercel.app"];
+
+app.options("*", cors());
+app.use(express.json()); // allow us to accept JSON data in the req.body
+app.use(
+  cors({
+    origin: origins,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+  });
+}
+
+connectDB();
+
+export default app;
